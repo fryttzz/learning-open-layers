258bdf8b81077672d9814fba1701c3067fe0c85d
Matheus Santos <matheusps0012@gmail.com>
2022-10-25 17:25:21 -0300

    Add - New Features

--- main.js
+++ main.js
@@ -2,30 +2,26 @@ var mapView = new ol.View({
     center: ol.proj.fromLonLat([-49.3833, -26.2400]),
     zoom: 13
 })
-
 var map = new ol.Map({
     target: "map",
     view: mapView
 })
-
 var noneTile = new ol.layer.Tile({
     title: "None",
     type: "base",
     visible: false
 })
-
 var osmTile = new ol.layer.Tile({
     title: "Open Street Map",
     visible: true,
     type: "base",
     source: new ol.source.OSM()
 })
-
 var baseGroup = new ol.layer.Group({
     title: "Base Maps",
-    layers: [noneTile, osmTile]
+    layers: [noneTile, osmTile],
+    fold: true
 })
-
 map.addLayer(baseGroup)
 
 var SBSLimite = new ol.layer.Tile({
@@ -37,7 +33,6 @@ var SBSLimite = new ol.layer.Tile({
         visible: true
     })
 })
-
 var SBSLinhas = new ol.layer.Tile({
     title: "Sistema de Linhas de São Bento do Sul",
     source: new ol.source.TileWMS({
@@ -47,22 +42,20 @@ var SBSLinhas = new ol.layer.Tile({
         visible: true
     })
 })
-
 var SBSTerminais = new ol.layer.Tile({
-    title: "Sistema de Linhas de São Bento do Sul",
+    title: "Terminais do Sistema de Linhas de São Bento do Sul",
     source: new ol.source.TileWMS({
         url: "http://192.168.1.166:8080/geoserver/sao_bento_do_sul/wms",
-        params: { "LAYERS": "sao_bento_do_sul:01_terminais", "TILED": true },
+        params: { "LAYERS": "sao_bento_do_sul:terminais", "TILED": true },
         serviceType: "geoserver",
         visible: true
     })
 })
-
 var overlaysGroup = new ol.layer.Group({
     title: "Overlays",
-    layers: [SBSLimite, SBSLinhas, SBSTerminais]
+    layers: [SBSLimite, SBSLinhas, SBSTerminais],
+    fold: true
 })
-
 map.addLayer(overlaysGroup)
 
 var layerSwitcher = new ol.control.LayerSwitcher({
@@ -70,5 +63,17 @@ var layerSwitcher = new ol.control.LayerSwitcher({
     startActive: false,
     groupSelectStyle: "children"
 })
-
 map.addControl(layerSwitcher)
+
+var mousePosition = new ol.control.MousePosition({
+    className: "mousePosition",
+    projection: "EPSG:4326",
+    coordinateFormat: function(coordinate) { return ol.coordinate.format(coordinate, `{y}, {x}`, 6) }
+})
+map.addControl(mousePosition)
+
+var scaleControl = new ol.control.ScaleLine({
+    bar: true,
+    text: true
+})
+map.addControl(scaleControl)
